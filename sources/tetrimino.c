dc38cbcdffe326031e69e3b12b87c26d3d705d34
Jonathan Strayer <jostraye@e1z2r5p46.42.us.org>
2017-10-08 21:18:24 -0700

    nouvelle version de filtre input

--- sources/tetrimino.c
+++ sources/tetrimino.c
@@ -6,7 +6,7 @@
 /*   By: pnizet <pnizet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/04 19:30:41 by pnizet            #+#    #+#             */
-/*   Updated: 2017/10/07 20:39:41 by jostraye         ###   ########.fr       */
+/*   Updated: 2017/10/08 21:17:34 by jostraye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 #include <unistd.h>
 
 /*
-** Verifie que les tetriminos ont une bonne shape general et retourne
-** le nombre de blocks
+** Verifie que les tetriminos ont une bonne shape general et verifie
+** s'il y a bien 4 '#' et 12 "."
 */
 
 int		check_shape(char *str)
@@ -39,7 +39,7 @@ int		check_shape(char *str)
 			k++;
 		i++;
 	}
-	if ((str[4] != '\n' && str[9] != '\n' && str[14] != '\n' && str[19] != '\n')
+	if ((str[4] != '\n' || str[9] != '\n' || str[14] != '\n' || str[19] != '\n')
 	|| (str[20] != '\0' && str[20] != '\n'))
 		return (0);
 	if (j != 4 || k != 12)
@@ -76,11 +76,27 @@ int		check_connections(char *str)
 		return (0);
 }
 
+int		check_last(char *str)
+{
+	int i;
+
+	printf("%s", str);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	if (str[i - 1] == '\n' && (str[i - 2] == '.' || str[i - 2] == '#'))
+		return (1);
+	else
+		return (0);
+
+}
+
 int main(int ac, char **av)
 {
 	int fd;
 	char *buf;
-
+	int block_nb;
+	char *str;
 	if (ac != 2)
 	{
 		printf("usage: fillit input_file\n");
@@ -93,7 +109,7 @@ int main(int ac, char **av)
 	** O_RDONLY stand for "Open for reading only"
 	*/
 
-
+	block_nb = 0;
 	buf = (char *)malloc(22);
 	fd = open(av[1], O_RDONLY);
 	while (read(fd, buf, 21) != 0)
@@ -108,6 +124,15 @@ int main(int ac, char **av)
 			printf("CX \n");
 		if (check_connections(buf) == 0 || check_shape(buf) == 0)
 			printf("error\n");
+		block_nb++;
+		printf("%d \n", block_nb);
 	}
+	fd = open(av[1], O_RDONLY);
+	str = (char *)malloc(22 * block_nb);
+	read(fd, str, 21 * block_nb);
+	if (check_last(str) == 0)
+		printf("error de fin\n");
+	else
+		printf("Hehe\n");
 	return (0);
 }
